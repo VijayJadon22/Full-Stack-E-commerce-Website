e65d2c4490dcabc45c032dab3b0b7b150c00576a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-22 12:34:24 +0530

    Added presave method in user schema to hash password and also compare password method for comparing passwords

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -38,7 +38,18 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password") || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
 
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 
 const User = mongoose.model("User", userSchema);
