31a05c2dd8083a58730c24b0faa516483ea2c1d6
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-27 18:11:48 +0530

    Implemented getCoupon controller function for egtting coupon

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,13 @@
+import Coupon from "../models/coupon.model.js"
+
+export const getCoupon = async (req, res) => {
+    try {
+        const coupon = await Coupon.findOne({ userId: req.user._id, isActive: true });
+        res.json(coupon || null);
+    } catch (error) {
+        console.log("Error in getCoupon controller: ", error);
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
+
+export 
