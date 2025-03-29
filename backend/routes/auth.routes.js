83c80508663400bfd00dde6b521d97eaac67d634
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-29 18:50:50 +0530

    Added getProfile route in auth routes for letting user to get his profile deatils and info

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,8 @@ import express from "express";
 
 // Import authentication controller functions from the auth.controller.js file
 // These functions handle the logic for signup, login, logout, and token refreshing
-import { signup, login, logout, refreshToken } from "../controllers/auth.controller.js";
+import { signup, login, logout, refreshToken, getProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -14,9 +15,9 @@ router.post("/logout", logout); // Route to handle user logout requests
 // This route provides a new access token if a valid refresh token is sent, by calling the 'refreshToken' controller function
 router.post("/refresh-token", refreshToken);
 
-// Route to handle user profile requests (commented out for now)
-// If uncommented, it would call a 'getProfile' controller function to retrieve user profile information
-// router.get("/profile", getProfile);
+// Route to handle user profile requests 
+// it would call a 'getProfile' controller function to retrieve user profile information
+router.get("/profile", protectRoute, getProfile);
 
 // Export the router so it can be used in other parts of the application
 export default router;
