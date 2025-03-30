d8ad64d12f4fff5a28b4076a2a06fd8016b1ce77
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-30 19:42:59 +0530

    Made few changes in axios configuration for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,11 +3,12 @@ import axios from "axios";
 
 // Creating a reusable Axios instance with predefined configurations
 const axiosInstance = axios.create({
-    // Setting the base URL for all HTTP requests. This acts as the root endpoint for your API.
-    baseURL: "http://localhost:5000/api",
+    // Dynamically setting the base URL depending on the environment:
+    // If the app is in development mode, use "http://localhost:5000/api" as the base URL.
+    // If in production mode, use "/api" (assuming the API is served from the same domain).
+    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
     withCredentials: true, // Enabling the sending of credentials (such as cookies) with cross-origin requests
 });
 
 // Exporting the Axios instance so it can be imported and used in other files
 export default axiosInstance;
- 
