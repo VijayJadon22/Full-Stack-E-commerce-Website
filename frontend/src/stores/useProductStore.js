2aa3c45d54f9330e048aa85ee7098ffd253fa0be
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-31 19:34:03 +0530

    Implemnted fetchAllProducts fro fetching all the products from database

--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -24,5 +24,16 @@ export const useProductStore = create((set, get) => ({
 
     },
 
+    fetchAllProducts: async () => {
+        set({ loading: true });
+        try {
+            const res = await axios.get("/products");
+            set({ products: res.data.products, loading: false });
+        } catch (error) {
+            set({ error: "Failed to fetch products", loading: false });
+            toast.error(error.response.data.message || "An error occurred");
+        }
+    },
+
     
 }))
