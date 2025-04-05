35589e7685ff329b3ad0cca2e356f9e13a840876
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-05 23:30:47 +0530

    Configured the project for production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
@@ -15,7 +16,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({limit:"5mb"})); //let us parse the body of the request, so that we can use req.body 
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" })); //let us parse the body of the request, so that we can use req.body 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -24,6 +27,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: http://localhost:${PORT}`);
